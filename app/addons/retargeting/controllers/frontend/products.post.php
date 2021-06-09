403bf4f97887d82442ec374676e94ca915f3569f
Alex B <80752424+AlexEAX@users.noreply.github.com>
2021-06-09 16:02:59 +0300

    Update products.post.php

--- app/addons/retargeting/controllers/frontend/products.post.php
+++ app/addons/retargeting/controllers/frontend/products.post.php
@@ -68,9 +68,9 @@ if ($mode == 'view' && !empty($_REQUEST['product_id'])) {
             ]
         ];
 
-        if ( floatval(PRODUCT_VERSION) < 4.9 ) {
-            $product_details['category'] = [ $product_details['category'] ];
-        }
+        /* if ( floatval(PRODUCT_VERSION) < 4.9 ) {} */
+        $product_details['category'] = [ $product_details['category'] ];
+        
         
         Registry::get('view')->assign('ra_product_info', json_encode($product_details, JSON_PRETTY_PRINT));
         Registry::get('view')->assign('ra_fullPrice', $price);
