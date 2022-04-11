5f92d3574e75c9d29f0346873a646c483cc1a223
Alex B <80752424+AlexEAX@users.noreply.github.com>
2022-04-11 11:13:29 +0300

    Update rtgcron.php

--- app/addons/retargeting/controllers/frontend/rtgcron.php
+++ app/addons/retargeting/controllers/frontend/rtgcron.php
@@ -6,8 +6,8 @@ use Tygh\Registry;
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 if (fn_retargeting_get_addon_variable('retargeting_discounts_api') === $_GET['key']) {
     $path = str_replace('app/www/app/addons/retargeting','',fn_dir());
-echo "<pre><code>0 */4 * * * /usr/bin/php ".$path."index.php  --dispatch=rtg-feed.cron >/dev/null 2>&1
+echo "<pre><code>0 */4 * * * /usr/bin/php ".$path."index.php  --dispatch=rtgfeed.cron >/dev/null 2>&1
 OR
-0 */4 * * * wget --quiet -O /dev/null ".fn_url("index.php?dispatch=rtg-feed.cron")."</code></pre>";
+0 */4 * * * wget --quiet -O /dev/null ".fn_url("index.php?dispatch=rtgfeed.cron")."</code></pre>";
     exit();
 }
